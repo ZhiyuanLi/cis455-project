3a87054213f0fa5a2fcba3b45a307be1c1ed24b5
Zhiyuan Li <zhil@seas.upenn.edu>
2016-04-16 18:50:32 -0400

    create servlet

---
+++ src/edu/upenn/cis455/servlet/SearchServlet.java
@@ -0,0 +1,24 @@
+package edu.upenn.cis455.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+/**
+ * Search Servlet
+ * @author zhiyuanli
+ *
+ */
+@SuppressWarnings("serial")
+public class SearchServlet extends HttpServlet{
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+}
