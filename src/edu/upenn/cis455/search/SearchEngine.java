c1a59a05f5a1bb5dc05acfe073509020c0694805
Zhiyuan Li <zhil@seas.upenn.edu>
2016-04-16 23:43:34 -0400

    initial jsp

---
+++ src/edu/upenn/cis455/search/SearchEngine.java
@@ -0,0 +1,19 @@
+package edu.upenn.cis455.search;
+
+public class SearchEngine {
+
+	private String query;
+	
+
+	public SearchEngine() {
+		// TODO Auto-generated constructor stub
+		init();
+	}
+
+	private void init() {
+
+	}
+	
+	
+
+}
--- src/edu/upenn/cis455/servlet/SearchServlet.java
+++ src/edu/upenn/cis455/servlet/SearchServlet.java
@@ -1,24 +1,40 @@
 package edu.upenn.cis455.servlet;
 
+import java.io.File;
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 /**
  * Search Servlet
+ * 
  * @author zhiyuanli
  *
  */
 @SuppressWarnings("serial")
-public class SearchServlet extends HttpServlet{
+public class SearchServlet extends HttpServlet {
+
+	private String jspResultPath;
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		
+		dispatchReply(req, resp);
 	}
-	
-	
+
+	@Override
+	public void init() throws ServletException {
+		// set worker status for this worker
+
+	}
+
+	public void dispatchReply(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		RequestDispatcher view = req.getRequestDispatcher(jspResultPath);
+		view.forward(req, resp);
+
+	}
 }
