c9569d4d041953fe338bbe254f5c7307c59d61d2
Zhiyuan Li <zhil@seas.upenn.edu>
2016-04-19 01:43:21 -0400

    HDFS wrapper read all files under one dir

--- src/edu/upenn/cis455/storage/HDFSWrapper.java
+++ src/edu/upenn/cis455/storage/HDFSWrapper.java
@@ -10,28 +10,40 @@ import org.apache.hadoop.io.*;
 import org.apache.hadoop.util.*;
 
 public class HDFSWrapper {
-	 public static void main(String[] args) throws IOException, URISyntaxException 
-	 {
-	  //1. Get the instance of Configuration
-	  Configuration configuration = new Configuration();
-	  //2. URI of the file to be read
-	  URI uri = new URI("hdfs://localhost:9000/user/input/index_input1");
-	  //3. Get the instance of the HDFS 
-	  FileSystem hdfs = FileSystem.get(uri, configuration);
-	  //4. A reference to hold the InputStream
-	  InputStream inputStream = null;
-	  try{
-	   //5. Prepare the Path, i.e similar to File class in Java, Path represents file in HDFS
-	   Path path = new Path(uri);
-	   //6. Open a Input Stream to read the data from HDFS
-	   inputStream = hdfs.open(path);
-	   //7. Use the IOUtils to flush the data from the file to console
-	   IOUtils.copyBytes(inputStream, System.out, 4096, false);
-	  }finally{
-	   //8. Close the InputStream once the data is read
-	   IOUtils.closeStream(inputStream);
-	  }
+	public static void main(String[] args) throws IOException, URISyntaxException {
+		// 1. Get the instance of Configuration
+		Configuration configuration = new Configuration();
+//		// 2. URI of the file to be read
+//		URI uri = new URI("hdfs://localhost:9000/user/input/inout_index1");
+//		// 3. Get the instance of the HDFS
+//		FileSystem hdfs = FileSystem.get(uri, configuration);
+//		// 4. A reference to hold the InputStream
+//		InputStream inputStream = null;
+//		try {
+//			// 5. Prepare the Path, i.e similar to File class in Java, Path
+//			// represents file in HDFS
+//			Path path = new Path(uri);
+//			// 6. Open a Input Stream to read the data from HDFS
+//			inputStream = hdfs.open(path);
+//			// 7. Use the IOUtils to flush the data from the file to console
+//			IOUtils.copyBytes(inputStream, System.out, 4096, false);
+//		} finally {
+//			// 8. Close the InputStream once the data is read
+//			IOUtils.closeStream(inputStream);
+//		}
+		configuration.addResource(new Path("/usr/local/Cellar/hadoop/2.7.1/libexec/etc/hadoop/core-site.xml"));
+		FileSystem fs = FileSystem.get(configuration);
+		FileStatus[] status = fs.listStatus(new Path("hdfs://localhost:9000/user/input/"));
+        for (int i=0;i<status.length;i++){
+                BufferedReader br=new BufferedReader(new InputStreamReader(fs.open(status[i].getPath())));
+                String line;
+                line=br.readLine();
+                while (line != null){
+                        System.out.println(line);
+                        line=br.readLine();
+                }
+        }
 
-	 }
+	}
 
 }
