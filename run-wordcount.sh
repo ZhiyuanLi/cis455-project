d1aaa7075a57c07889685b0d50c73580f803bd4e
Zhiyuan Li <zhil@seas.upenn.edu>
2016-04-19 23:00:59 -0400

    remove all class files

--- run-wordcount.sh
+++
@@ -1,9 +0,0 @@
-/usr/local/bin/hdfs dfs -rm -r output
-/usr/local/bin/hadoop jar build/jar/WordCount.jar input/ output
-echo 'Job Input1'
-echo '----------'
-echo ''
-echo ''
-echo 'Job Output1'
-echo '----------'
-/usr/local/bin/hadoop dfs -cat output/part-00000
