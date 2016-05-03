b350d9038f5586b7282c2a4ab5afc692bbb51877
woodi kelsey <woody@hntvlan569.3363.wlan.wireless-resnet.upenn.edu>
2016-05-03 17:36:13 -0400

    add image

---
+++ src/edu/upenn/cis455/search/ImageSearchWorker.java
@@ -0,0 +1,53 @@
+package edu.upenn.cis455.search;
+
+import java.util.List;
+
+import edu.upenn.cis455.storage.DynamoDBWrapper;
+import edu.upenn.cis455.storage.ImageContent;
+
+public class ImageSearchWorker implements Runnable{
+	
+	/**
+	 * Instance for TitleSearchWorker
+	 */
+	private DynamoDBWrapper db;
+	private String word;
+	private List<ImageContent> imageItems;
+	
+	/**
+	 * Constructor for TitleSearchWorker
+	 * 
+	 * @param db
+	 * @param word
+	 */
+	public ImageSearchWorker(DynamoDBWrapper db, String word) {
+		this.db = db;
+		this.word = word;
+		this.imageItems = null;
+	}
+
+	@Override
+	public void run() {
+		imageItems = db.getImageContentQuery(word);
+	}
+
+	/**
+	 * Get the word
+	 * 
+	 * @return the word
+	 */
+	public String getWord() {
+		return word;
+	}
+
+	/**
+	 * Get the title items
+	 * 
+	 * @return the titleItems
+	 */
+	public List<ImageContent> getImageItems() {
+		return imageItems;
+	}
+
+
+}
